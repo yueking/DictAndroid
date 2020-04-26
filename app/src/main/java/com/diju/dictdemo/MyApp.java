ecbe73c95226be3b5ef830106e29e8bc301250b5
yuewu <yuekinger@gmail.com>
2020-04-26 16:40:08 +0800

    init

--- app/src/main/java/com/diju/dictdemo/MyApp.java
+++ app/src/main/java/com/diju/dictdemo/MyApp.java
@@ -8,31 +8,9 @@ public class MyApp extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-
         /**
-         * 预先加载一级列表所有城市的数据
-         */
-//        DictListLoader.getInstance().loadCityData(this);
-
-        /**
-         * 预先加载三级列表显示省市区的数据
-         */
-//        DictListLoader.getInstance().loadProData(this);
-
-        /**
-         * 预先加载字典数据
+         * 程序启动加载字典数据
          */
         DictListLoader.getInstance().loadDictData(this);
-//        refWatcher = LeakCanary.install(this);
     }
-
-
-    //在自己的Application中添加如下代码
-//    private RefWatcher refWatcher;
-
-    //在自己的Application中添加如下代码
-//    public static RefWatcher getRefWatcher(Context context) {
-//        MyApp application = (MyApp) context.getApplicationContext();
-//        return application.refWatcher;
-//    }
 }
--- app/src/main/java/com/diju/dictdemo/dict/Constant.java
+++ app/src/main/java/com/diju/dictdemo/dict/Constant.java
@@ -1,6 +1,5 @@
 package com.diju.dictdemo.dict;
 
 public class Constant {
-    public static final String CITY_DATA = "china_city_data.json";
     public static final String DICT_DATA = "dict.json";
 }
